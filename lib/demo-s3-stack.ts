11ed4f0fd8fea3362bd785c424d2c508977e86b7
annts095 <anan095.k@gmail.com>
2022-12-13 16:32:42 +0900

    refactor format

--- lib/demo-s3-stack.ts
+++ lib/demo-s3-stack.ts
@@ -1,12 +1,12 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class DemoS3Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-     new cdk.aws_s3.Bucket(this, 'demoBucket', {
-      bucketName: 'cdk-demo-bucket-20221129',
+    new cdk.aws_s3.Bucket(this, "demoBucket", {
+      bucketName: "cdk-demo-bucket-20221129",
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       publicReadAccess: true,
       websiteIndexDocument: "index.html",
